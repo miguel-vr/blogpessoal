8653dff4d414d44dcff29f1cade145aa1ba92001
miguel <miguelvr1202@gmail.com>
2022-04-06 19:16:46 -0300

    ADD Security/Service

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -17,40 +17,44 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//3- colocar a @entity = entidade, javax. e @Table(name = "tb_nomedatabela")
-// CREATE TABLE tb_postagens
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-	// 4- @Id indica a chave primaria
-	// 4- @GenetedValue = auto increment
-	// 1-coloque o private com os atributos
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id; 
 
-	// @NotNull = NOTNULL
-	// @NotBlank = NOT NULL (mensagem para o usuario)
-	// @Size = definir os parametros de tamanho
-	@NotBlank(message = "O atributo título é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo titulo deve possuir no min 5 e no max 100 caracteres")
-	private String titulo;
+	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo; 
 
 	@NotNull(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 1000, message = "O atributo texto deve possuir no min 10 e no max 1000 caracteres")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
 	private String texto;
 
-	// @UpdateTimestamp = atualizar/criar a data e hora da postagem automaticamente
 	@UpdateTimestamp
 	private LocalDateTime data;
 
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-	
-	
-	// 2-criando get/set
+
+	/**
+	 * Relacionamento com a classe Usuario
+	 * Não esqueça de criar os métodos getters e setters para o atributo usuario.
+	 */
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	/**
+	 * 
+	 * Os Métodos Get e Set obrigatoriamente devem ser criados para todos os atributos
+     * da Classe, inclusive os novos atributos que forem adicionados no decorrer do
+     * processo de Desenvolvimento.
+	 * 
+	 */	
 	public Long getId() {
 		return id;
 	}
@@ -84,11 +88,23 @@ public class Postagem {
 	}
 
 	public Tema getTema() {
-		return tema;
+		return this.tema;
 	}
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	/**
+	 * Métodos Get e Set para o atributo usuario
+	 */
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 	
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -7,19 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
-//1-criar a interface da postagem, metodos para implementar na classe.
-//2-@Repository falando que é repository
-// Erdando a jpaRepository <passando os dados pra manipular> 
-// Long = id, ele recebe o id
-// dando acesso aos metodos para o repository manipular
-// postagem = tabela, Long = id
-
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	// criando busca por titulo
-	// Uma list de da tabela postagem, procure tudo que contenha titulo
-	// IgnoreCase ignora maiúsculo e minúsculo
-	// SELECT * FROM tb_postagens WHERE titulo LIKE "%titulo%";
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
